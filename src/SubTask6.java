08d9f98301f521db65febcf195dc6053aebb081f
Maxim Nebo <gp.max.dev@gmail.com>
2022-12-13 20:14:28 +0200

    all work

--- src/SubTask6.java
+++ src/SubTask6.java
@@ -1,66 +1,55 @@
 public class SubTask6 {
     public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    final static double ALPHABET_COUNT = 26;
     public static void main(String[] args) {
 
     }
-
     private static int getNumberOfChar(char cha) {
         String upperCaseChar = String.valueOf(cha).
-                                     trim()
-                                    .toUpperCase();
+                trim()
+                .toUpperCase();
         return alphabet.indexOf(upperCaseChar) + 1;
     }
-
     private static int getNumberOfColumn(String columnName) {
 
         int result = 0;
+
+        double degree;
 
         if (!columnName.isEmpty()) {
             char[] inputCharArray = columnName.toCharArray();
 
             if (inputCharArray.length == 1) {
-               result = getNumberOfChar(inputCharArray[0]);
+                result = getNumberOfChar(inputCharArray[0]);
             } else {
-               result = (getNumberOfChar(inputCharArray[0]) * 26) + getNumberOfChar(inputCharArray[1]);
+                degree = inputCharArray.length - 1;
+                for(int i = 0; i < (inputCharArray.length - 1); i++)
+                {
+                    result += (getNumberOfChar(inputCharArray[i]) *  Math.pow(ALPHABET_COUNT, degree));
+                    degree -= 1;
+                }
+                result += getNumberOfChar(inputCharArray[inputCharArray.length - 1]);
             }
         }
         return result;
     }
 
-    private static String getStringNameOfColum(int numberOfColumn){
+    private static String getStringNameOfColum(int numberOfColumn) {
 
-        int firstIndexOfChar;
-        int secondIndexOfChar;
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if(numberOfColumn > 0 && numberOfColumn <= 26){
-            stringBuilder.append(alphabet.charAt(numberOfColumn - 1));
-
-        } else if (numberOfColumn > 26) {
-            firstIndexOfChar = numberOfColumn / 26;
-            secondIndexOfChar = (numberOfColumn % 26);
-            if(secondIndexOfChar == 0){
-                firstIndexOfChar -=1;
-                secondIndexOfChar = 26;
-            }
-            stringBuilder.append(alphabet.charAt(firstIndexOfChar - 1));
-            stringBuilder.append(alphabet.charAt(secondIndexOfChar - 1));
-
+        StringBuilder sb = new StringBuilder();
+        while (numberOfColumn-- > 0) {
+            sb.append(alphabet.charAt((int) (numberOfColumn % ALPHABET_COUNT)));
+            numberOfColumn /= ALPHABET_COUNT;
         }
-
-        return stringBuilder.toString().toUpperCase();
+        return sb.reverse().toString();
     }
 
     private static int getNumberOfRightColumn(String columnName){
         return getNumberOfColumn(columnName) + 1;
-
     }
 
-
 }
-
 
 
 
